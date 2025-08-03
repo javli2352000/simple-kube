6201a9a06c469c18aa4fa003cb0d806f433f37a5
javli2352000 <155173738+javli2352000@users.noreply.github.com>
2025-08-03 14:46:32 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'database-1.clecii4scanf.ap-south-1.rds.amazonaws.com';
+$db   = 'userdata';
+$user = 'prajwal';
+$pass = 'Prajwal23';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
